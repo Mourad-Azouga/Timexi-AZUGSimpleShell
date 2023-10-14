34fbfddd220af5cd021ee1a0f22585f5d1128bfd
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-10-14 06:48:54 -0700

    There is an issue with the pathfinder will be resolved soon

--- shell.c
+++ shell.c
@@ -1,16 +1,55 @@
 #include "shell.h"
+
 char *input = NULL;
+
 /**
- * sigint_handler - hanldes signal inputs
- * @signum: signal number
+ * pathfinder - a Nissan 4x4 truck, joking it looks for the path
+ * @input: the command to be looked for
+ * Return: full path
  */
-void sigint_handler(int signum)
+char *pathfinder(char *input)
 {
-	if (signum == SIGINT)
-	{
-		free(input);
-		exit(EXIT_SUCCESS);
-	}
+    char *path_env = getenv("PATH");
+    char *path_copy = strdup(path_env);
+    char *path_dir = NULL;
+    char *full_path = NULL, *full_path_copy = NULL;
+
+    if (path_copy == NULL)
+    {
+        perror("strdup error");
+        exit(EXIT_FAILURE);
+    }
+
+    path_dir = strtok(path_copy, ":");
+    while (path_dir)
+    {
+        full_path = malloc(strlen(path_dir) + strlen(input) + 2);
+        if (full_path == NULL)
+        {
+            perror("malloc fullpath error");
+            exit(EXIT_FAILURE);
+        }
+
+        full_path[0] = '\0';
+        strcpy(full_path, path_dir);
+        strcat(full_path, "/");
+        strcat(full_path, input);
+
+        if (access(full_path, F_OK) == 0)
+        {
+            free(path_copy);
+            full_path_copy = full_path;
+            full_path = NULL;
+            return (full_path_copy);
+        }
+
+        free(full_path);
+        full_path = NULL;
+        path_dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return (NULL);
 }
 
 /**
@@ -25,7 +64,6 @@ ssize_t gtln;
 int i = 0, status = 0, j = 0, k = 0;
 pid_t child;
 
-signal(SIGINT, sigint_handler);
 while (1)
 {
 if (isatty(STDIN_FILENO))
@@ -57,8 +95,6 @@ if (isatty(STDIN_FILENO))
 			full_path = NULL;
 			free(full_path);
 		}
-		if (access(argv[0], X_OK) == 0)
-		{
 		child = fork();
 		if (child == -1)
 		{
@@ -69,16 +105,11 @@ if (isatty(STDIN_FILENO))
 			if (child == 0)
 			{
 			execute(argv);
-			exit(EXIT_FAILURE);
+			exit(0);
 			}
 			else
 			{
 				waitpid(child, &status, WUNTRACED);
-			}
-		}
-		else
-			{
-				perror(full_path);
 			}
 		for (j = 0; argv[j] != NULL; j++)
 		{
@@ -92,7 +123,7 @@ for (k = 0; argv[k] != NULL; i++)
 	{
 	free(argv[k]);
 	}
-return (0);
+return (EXIT_SUCCESS);
 }
 /**
  * execute - executes the argv from main
