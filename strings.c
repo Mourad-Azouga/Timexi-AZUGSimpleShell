7e6c62926d8d79175aadc14c961b6079e0aba455
StephanSerwaa <aduseistephanie50@gmail.com>
2023-10-17 08:47:42 -0700

    adds

---
+++ strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to calculate the length of a string
+int stringLength(const char *str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
