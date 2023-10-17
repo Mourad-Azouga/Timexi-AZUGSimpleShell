64cb6c9b564e2167ab3a8b1d2f45aa480dcf4afc
StephanSerwaa <aduseistephanie50@gmail.com>
2023-10-17 08:15:07 -0700

    add

--- strings.c
+++ strings.c
@@ -26,26 +26,4 @@ void stringCopy(char *dest, const char *src) {
         dest[i] = src[i];
         i++;
     }
-    dest[i] = '\0';
-}
-
-/**
- * _strcat- function to concatenate two strings
-*@s: strings to concatenate 
-*Return: concatenate
-*/
-
-void stringConcat(char *dest, const char *src) {
-    int destLength = stringLength(dest);
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[destLength + i] = src[i];
-        i++;
-    }
-    dest[destLength + i] = '\0';
-}
-
-/Function to compare two strings, returns 0 if equal, <0 if str1 < str2, and >0 if str1 > str2
-int stringCompare(const char *str1, const char *str2) {
-    return strcmp(str1, str2);
-}
+    dest[i
