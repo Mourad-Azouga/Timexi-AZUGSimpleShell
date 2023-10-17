5406b000b6208c8859af4e0b3f70af30885657db
StephanSerwaa <aduseistephanie50@gmail.com>
2023-10-17 08:36:31 -0700

    adss

--- gert.c
+++ gert.c
@@ -62,18 +62,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / hell.h
+		 hell.h
 
 /**
  * main - Lists all the natural numbers below 1024 (excluded)
