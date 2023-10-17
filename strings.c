17b3c9fe89d8aa08d9c2856d0672b7fd3d705306
StephanSerwaa <aduseistephanie50@gmail.com>
2023-10-17 08:16:29 -0700

    adds

--- strings.c
+++ strings.c
@@ -11,19 +11,6 @@ int stringLength(const char *str) {
     while (str[length] != '\0') {
         length++;
     }
-    return length;
-}
-
-/**
- * _strcp - function to copy a string from source to destination
-*@s: strrings to copy 
-*Return: copying
-*/
-
-void stringCopy(char *dest, const char *src) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
+    return +;
     }
     dest[i
